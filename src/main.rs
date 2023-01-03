23377323f973be4d5b16b67567c63c613b482e13
netherous <georgejqy@gmail.com>
2023-01-03 14:35:03 -0600

    Added translate_addr function in the memory module

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,40 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     yios::init();
 
     use x86_64::VirtAddr;
-    use yios::memory::active_level_4_table;
+    use yios::memory::{translate_addr,active_level_4_table};
+    use x86_64::structures::paging::PageTable;
     
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe {active_level_4_table(phys_mem_offset)};
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
 
-    for(i, entry) in l4_table.iter().enumerate(){
-        if !entry.is_unused(){
-            println!("L4 Entry {}: {:?}", i, entry);
-        }
+    for &address in &addresses{
+        let virt = VirtAddr::new(address);
+        let phys = unsafe {translate_addr(virt,phys_mem_offset)};
+        println!("{:?}-> {:?}", virt, phys);
     }
+
+    // let l4_table = unsafe {active_level_4_table(phys_mem_offset)};
+    //
+    // for(i, entry) in l4_table.iter().enumerate(){
+    //     if !entry.is_unused(){
+    //         println!("L4 Entry {}: {:?}", i, entry);
+    //
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //         let ptr: *const PageTable= VirtAddr::new(virt).as_mut_ptr();
+    //         let l3_table:&PageTable = unsafe{&*ptr};
+    //         for(i,entry) in l3_table.iter().enumerate(){
+    //             if !entry.is_unused(){
+    //                 println!("  L3 Entry {}: {:?}", i,entry);
+    //             }
+    //         }
+    //     }
+    // }
     #[cfg(test)]  
     test_main();
 
